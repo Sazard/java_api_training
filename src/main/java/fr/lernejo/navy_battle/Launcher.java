a8c7b401807362eef5ce1583ef516209f144e3d5
nicolas.sutter <nicolas.sutter@epita.fr>
2021-05-27 13:58:45 +0200

    request handler and start server

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,33 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Launcher {
     public static void main(String[] args) {
-        
+        try {
+            int arg = Integer.parseInt(args[0]);
+            HttpServer serv = HttpServer.create(new InetSocketAddress(arg), 0);
+            serv.setExecutor(Executors.newSingleThreadExecutor());
+            serv.createContext("/ping", new RequestHandler());
+            serv.start();
+
+        }
+        catch (java.io.IOException e){
+            System.err.println("java.io.IOException " + e.getMessage());
+        }
+        /*
+        catch (java.net.BindException a) {
+            System.err.println("java.net.BindException " + a.getMessage());
+        }*/
+
     }
 }
---
+++ src/main/java/fr/lernejo/navy_battle/RequestHandler.java
@@ -0,0 +1,21 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class RequestHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String body = "Hello";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(body.getBytes());
+        }
+        catch (IOException ex) {
+            System.err.println("IOException "+ ex.getMessage());
+        }
+    }
+}
